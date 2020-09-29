289b9551a22521e8fb35c75ec6d8928a0b86188e
Jovanny Cruz <jovannypcg@gmail.com>
2020-09-29 14:32:29 -0500

    Add function to draw canvas

--- src/app.js
+++ src/app.js
@@ -9,4 +9,21 @@ export class App {
   fetchLocations() {
     alert('Siii');
   }
+
+  plotImageData(ctx, binaryPixelArray) {
+    const pixelArray = binaryPixelArray
+      .map(v => {
+        const pixel = v ? 255 : 0;
+        const color = [pixel, pixel, pixel];
+        return [...color, 255];
+      })
+      .flat();
+    const pictureSize = Math.sqrt(binaryPixelArray.length);
+    const imageData = new ImageData(
+      new Uint8ClampedArray(pixelArray),
+      pictureSize,
+      pictureSize
+    );
+    ctx.putImageData(imageData, 0, 0);
+  }
 }
